6292abf90757f1bb2638e7081e046b9ae4c3c106
Ben Savage <Finnegan2100@protonmail.com>
2019-06-09 16:42:12 -0700

    basic wrench turning mechanism complete

--- Assets/Scripts/InteractionManager.cs
+++ Assets/Scripts/InteractionManager.cs
@@ -7,26 +7,80 @@ public class InteractionManager : MonoBehaviour
     public GameObject handContainer;
     public GameObject leftHand;
     public GameObject wrench;
+    public GameObject bolt;
+
+    public bool wrenchAffixedToHand = false;
+    public bool handConstrainedByWrench = false;
+
+    private bool havePlayedWrenchTurnSFX = false;
 
     private RaycastHit hit;
     private RaycastHit handHit;
 
     private Vector3 forward;
 
+    private Vector3 newWrenchBoxColliderPosition = new Vector3(5, 0, 0);
+    private Vector3 newWrenchBoxColliderSize = new Vector3(40, 60, 15);
+
+    private float handShiftX = 4.0f;
+    private float handShiftY = 1.6f;
+
     private bool havePerformedOutsideGrab = false;
     private bool wrenchInTargetPosition = false;
     private bool haveRotatedWrench = false;
-    public bool wrenchAffixedToHand = false;
     private int wrenchRotationYAmount = -90;
 
     void Update()
     {
         MoveHandWithCursor();
         TriggerGrabOnClick();
+        PerformGrabOnWrench();
+        AffixWrenchToHand();
+        ConstrainHandByWrench();
+    }
 
+    //If hand is not yet locked into wrench turning position, move hand with mouse cursor.
+    private void MoveHandWithCursor()
+    {
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        {
+            if (!handConstrainedByWrench)
+            {
+                leftHand.transform.position = new Vector3(hit.point.x, hit.point.y, hit.point.z - 1);
+            }
+        }
+    }
 
-        forward = handContainer.transform.TransformDirection(Vector3.forward) * 20;
-        if (Physics.Raycast(leftHand.transform.position, forward, out handHit)) {
+    //If raycast has not targeted wrench, mouse click to see hand closing animation
+    private void TriggerGrabOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!havePerformedOutsideGrab)
+            {
+                handContainer.GetComponent<Animator>().SetTrigger("PerformGrab");
+                havePerformedOutsideGrab = true;
+                StartCoroutine("ResetOutsideGrab");
+            }
+        }
+    }
+
+    //Reset hand animation after closing
+    private IEnumerator ResetOutsideGrab()
+    {
+        yield return new WaitForSeconds(0.1f);
+        havePerformedOutsideGrab = false;
+    }
+
+    //Shoot raycast forward from hand. If raycast from hits wrench and mouse clicked, hand locks onto wrench
+    private void PerformGrabOnWrench()
+    {
+        int raycastLength = 20;
+
+        forward = handContainer.transform.TransformDirection(Vector3.forward) * raycastLength;
+
+        if (Physics.Raycast(leftHand.transform.position, forward, out handHit))
+        {
             wrenchInTargetPosition = true;
 
             if (Input.GetMouseButtonDown(0) && !wrenchAffixedToHand)
@@ -35,66 +89,64 @@ public class InteractionManager : MonoBehaviour
                 wrenchAffixedToHand = true;
             }
         }
+    }
 
+    //Attaches the wrench to the hand
+    private void AffixWrenchToHand()
+    {
         if (wrenchAffixedToHand)
         {
-
             if (!haveRotatedWrench)
             {
                 AlignWrenchWithHand();
             }
-                wrench.transform.position = new Vector3(leftHand.transform.position.x + 4, leftHand.transform.position.y + 1.6f, leftHand.transform.position.z);
+            wrench.transform.position = new Vector3(leftHand.transform.position.x + handShiftX, leftHand.transform.position.y + handShiftY, leftHand.transform.position.z);
         }
     }
 
-
-
-
-
-
-
-
-
-
-    private void TriggerGrabOnClick()
+    //Makes hand and wrench rotate around bolt
+    private void ConstrainHandByWrench()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (handConstrainedByWrench)
         {
-            if (!havePerformedOutsideGrab)
+            leftHand.transform.RotateAround(bolt.transform.position, Vector3.back, Input.GetAxis("Mouse X") * 4);
+            wrench.transform.RotateAround(bolt.transform.position, Vector3.back, Input.GetAxis("Mouse X") * 4);
+
+            TurnBolt();
+        }
+    }
+
+    //Screws in bolt according to force applied when moving mouse right.
+    private void TurnBolt()
+    {
+        if (Input.GetAxis("Mouse X") > 0)
+        {
+            if (bolt.transform.position.z <= 9.78)
             {
-                PerformGrabAnimation();
-                havePerformedOutsideGrab = true;
-                StartCoroutine("ResetOutsideGrab");
+                bolt.transform.position += new Vector3(0, 0, 0.008f);
+
+                if (!havePlayedWrenchTurnSFX)
+                {
+                    this.GetComponent<AudioSource>().Play();
+                    havePlayedWrenchTurnSFX = true;
+                }
             }
         }
-    }
-
-    private IEnumerator ResetOutsideGrab()
-    {
-        yield return new WaitForSeconds(0.1f);
-        havePerformedOutsideGrab = false;
-    }
-
-    private void MoveHandWithCursor()
-    {
-        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+        else
         {
-            leftHand.transform.position = hit.point;
+            havePlayedWrenchTurnSFX = false;
         }
     }
 
-    private void PerformGrabAnimation()
-    {
-        handContainer.GetComponent<Animator>().SetTrigger("PerformGrab");
-    }
-
+    //Flips wrench up to correct position when inside hand, removes box collider, then
+    //creates a new, smaller one for interaction with the bolt.
     private void AlignWrenchWithHand()
     {
         Destroy(wrench.GetComponent<Collider>());
         wrench.transform.Rotate(wrenchRotationYAmount, 0, 0, Space.World);
         BoxCollider collider = wrench.AddComponent<BoxCollider>();
-        collider.center = new Vector3(5, 0, 0);
-        collider.size = new Vector3(40, 60, 15);
+        collider.center = newWrenchBoxColliderPosition;
+        collider.size = newWrenchBoxColliderSize;
         haveRotatedWrench = true;
     }
 }
--- Assets/Scripts/WrenchManipulation.cs
+++ Assets/Scripts/WrenchManipulation.cs
@@ -6,26 +6,39 @@ public class WrenchManipulation : MonoBehaviour
 {
     public GameObject bolt;
     public GameObject cam;
-    private bool attachWrenchToBolt = false;
+    public GameObject hand;
 
+
+    private float wrenchShiftX = 0.2f;
+    private float wrenchShiftZ = 0.6f;
+
+    private bool attachWrenchToBolt = false;
 
     public void Update()
     {
-        if (attachWrenchToBolt)
-        {
-            this.transform.position = bolt.transform.position;
-        }
-
-        Debug.Log(this.transform.position);
+        AttachWrenchToBolt();
     }
 
+    //Snaps the wrench to the bolt
+    private void AttachWrenchToBolt()
+    {
+        if (attachWrenchToBolt)
+        {
+            this.transform.position = new Vector3(bolt.transform.position.x + wrenchShiftX, bolt.transform.position.y, bolt.transform.position.z - wrenchShiftZ);
+        }
+    }
+
+    //Sets specific constraints on the wrench's rigidbody to keep it from reacting after collision with bolt
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Bolt")
         {
             cam.GetComponent<InteractionManager>().wrenchAffixedToHand = false;
             attachWrenchToBolt = true;
-
+            cam.GetComponent<InteractionManager>().handConstrainedByWrench = true;
+            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.None;
+            this.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionY;
+            this.GetComponent<AudioSource>().Play();
         }
     }
 }
